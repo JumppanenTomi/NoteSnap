5bdc2c807c34f013d00716fde7db8cf0d0a4a77a
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2023-10-11 22:29:08 +0300

    delete should work now

--- app/src/main/java/fi/notesnap/notesnap/views/noteListingView.kt
+++ app/src/main/java/fi/notesnap/notesnap/views/noteListingView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
